d847862796d89a5db24c504d8eff951b92e3b2d3
틈틈 <teumteumee@gmail.com>
2025-02-28 15:54:37 +0900

    [스택,큐,데크] <2월 28일>

---
+++ 02_스택_큐_데크_과제/필수/4949.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool cmp(const string& se) {
+    stack<char> s;
+
+    for (char c : se) {
+        if (c == '(' || c == '[') {
+            s.push(c);
+        }
+        else if (c == ')') {
+            if (s.empty() || s.top() != '(') return false;
+            s.pop();
+        }
+        else if (c == ']') {
+            if (s.empty() || s.top() != '[') return false;
+            s.pop();
+        }
+    }
+
+    return s.empty();
+}
+
+int main() {
+    string se;
+
+    while (true) {
+        getline(cin, se);
+        if (se == ".") break;
+        cout << (cmp(se) ? "yes" : "no") << endl;
+    }
+
+    return 0;
+}
