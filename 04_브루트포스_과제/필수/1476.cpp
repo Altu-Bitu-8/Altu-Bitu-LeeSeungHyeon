3c075e9815d5ea0a6c9a5413d1cfa7cba8c46bf6
틈틈 <teumteumee@gmail.com>
2025-03-14 07:07:59 +0900

    [브루트포스]<3월 14일>

---
+++ 04_브루트포스_과제/필수/1476.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+    // 입력
+    int e, s, m;
+    cin >> e >> s >> m;
+    int year = 1;
+
+    // 나머지 연산
+    while (true) {
+        if ((year - 1) % 15 + 1 == e && (year - 1) % 28 + 1 == s && (year - 1) % 19 + 1 == m)
+        {
+            break;
+        }
+        year++;
+    }
+
+    cout << year << "\n";
+    return 0;
+}
