71ea1cb9e4de5cc741eef631f5f17cedda8f2ca8
틈틈 <teumteumee@gmail.com>
2025-04-04 01:04:07 +0900

    [동적 계획법] <4월 4일>

---
+++ 07_동적_계획법_과제/필수/11053.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+int solvedLen(int n, vector<int>& arr, vector<int>& dp) {
+	int ans = 0;
+
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[j] < arr[i]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		ans = max(ans, dp[i]);
+	}
+	
+	return ans;
+}
+
+int main() {
+	
+	int n;
+	cin >> n;
+	vector<int> arr(n);
+	vector<int> dp(n, 1);
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	int result = solvedLen(n, arr, dp);
+	cout << result << "\n";
+
+	return 0;
+}
