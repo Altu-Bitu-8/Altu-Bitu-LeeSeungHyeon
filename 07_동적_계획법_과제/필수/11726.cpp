4917efa3d4c24bca59238dc826cda6d8bcfc050d
이승현 <108600956+teumYee@users.noreply.github.com>
2025-04-10 00:30:27 +0900

    11726 피드백 반영

--- 07_동적_계획법_과제/필수/11726.cpp
+++ 07_동적_계획법_과제/필수/11726.cpp
@@ -1,30 +1,24 @@
-﻿#include <iostream>
+#include <iostream>
 #include <vector>
 using namespace std;
 
-int solvedMod(int n, vector<int> &dp) {
-	int ans = 0;
+int solvedMod(int n) {
+    const int MODNUM = 10007;  
+    vector<int> dp(n + 1);     
+    dp[1] = 1;
+    dp[2] = 2;
 
-	dp[1] = 1;
-	dp[2] = 2;
+    for (int i = 3; i <= n; i++) {
+        dp[i] = (dp[i - 1] + dp[i - 2]) % MODNUM;  // MODNUM으로 나누기
+    }
 
-	for (int i = 3; i <= n; i++) {
-		dp[i] = dp[i - 1] + dp[i - 2];
-	}
-	
-	return dp[n] % 10007;
+    return dp[n];
 }
 
 int main() {
-	
-	int n;
-	cin >> n;
+    int n;
+    cin >> n;
 
-	// 벡터 크기 확보하기
-	int size = (n < 3) ? 3 : n + 1;
-	//2*n 크기 채우는 경우의 수 dp
-	vector<int> dp(size);
-	cout << solvedMod(n,dp) << "\n";
-
-	return 0;
+    cout << solvedMod(n) << "\n";
+    return 0;
 }
